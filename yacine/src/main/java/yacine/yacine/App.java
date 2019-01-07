a7c7f12d3850f51645d54b09798f7997657fc9bd
Yacine AMOUCHE <yacine.amouche@yahoo.fr>
2019-01-07 19:29:52 +0100

    commit

---
+++ yacine/src/main/java/yacine/yacine/App.java
@@ -0,0 +1,13 @@
+package yacine.yacine;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
